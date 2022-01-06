cdb9030505af32a110928a72e57fa698e6f139fc
KARTHIK-KG <68492042+KARTHIK-KG@users.noreply.github.com>
2022-01-06 15:38:03 +0530

    Add files via upload

---
+++ C Programs/Exp 5.c
@@ -0,0 +1,28 @@
+// C Program to find sum of elements using array
+
+// I.Input number of elements in array
+// II.Input the elements using array
+// III.Create a function and find the sum of array elements using loop
+// IV.Display the sum
+
+#include<stdio.h>
+int sum(int arr[], int n)
+{
+    int sum = 0; // initialize sum
+
+    for (int i = 0; i < n; i++)
+    sum += arr[i];
+
+    return sum;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,40,50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("%d", sum(arr, n));
+    return 0;
+}
+
+// Input : 10  20  30  40  50
+// Output : 150
