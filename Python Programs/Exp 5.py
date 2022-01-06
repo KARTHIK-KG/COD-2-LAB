39d11845190a55a4e9f723843752f3d2a2cd8687
KARTHIK-KG <68492042+KARTHIK-KG@users.noreply.github.com>
2022-01-06 15:49:36 +0530

    Add files via upload

---
+++ Python Programs/Exp 5.py
@@ -0,0 +1,23 @@
+# Python Program to find sum of elements using Array
+
+# I.Define a function to calculate sum of array elements using loop
+# II.Create an array
+# III.Input number of elements and array elements using loop
+# IV.Invoke the function
+
+# No of elements of array: 5
+# Enter the array elements: 10 20 30 40 50
+
+def _sum(arr,n):
+        return(sum(arr))
+arr=[]
+# input values to list
+arr = [10,20,30,40,50]
+# calculating length of array
+n = len(arr)
+ans = _sum(arr,n)
+# display sum
+print (ans)
+
+# Input : 10 20 30 40 50
+# Output : 150
