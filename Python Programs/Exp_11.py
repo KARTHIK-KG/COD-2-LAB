eb5a7cc641619b4852202ef0d1bb6c96a89b817b
KARTHIK-KG <68492042+KARTHIK-KG@users.noreply.github.com>
2022-02-16 16:08:36 +0530

    Add files via upload

---
+++ Python Programs/Exp_11.py
@@ -0,0 +1,40 @@
+# Python Program for Insertion Sort
+
+# Eg:
+# n = 7 
+# Array Elements = {70, 50, 30, 10, 60, 40, 20}
+# Sorted Array = {10, 20, 30, 40, 50, 60, 70}
+
+def insertion_sort(T1):
+    for i in range(1,len(T1)):
+        temp = T1[i]
+        j = i - 1
+        while (j >= 0 and temp < T1[j]):
+            T1[j + 1] = T1[j]
+            j = j - 1
+        T1[j + 1]=temp
+n=int(input())
+T1=input().split()
+T2=T1
+T1 = [int(x) for x in T1]
+insertion_sort(T1)
+for i in range(len(T1)):
+   print (T1[i],end=' ')
+
+# Test Case 1 :
+
+# Input :
+# 7
+# 70 50 30 10 60 40 20
+
+# Output :
+# 10 20 30 40 50 60 70
+
+# Test Case 2 :
+
+# Input :
+# 6
+# 66 44 22 11 33 55
+
+# Output :
+# 11 22 33 44 55 66
