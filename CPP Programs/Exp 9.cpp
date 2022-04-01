4844ce1923c296a6b22f827b487b91a47f5e85a6
KARTHIK-KG <68492042+KARTHIK-KG@users.noreply.github.com>
2022-04-01 20:28:48 +0530

    Update Exp 9.cpp

--- CPP Programs/Exp 9.cpp
+++ CPP Programs/Exp 9.cpp
@@ -6,57 +6,58 @@
 
 #include<iostream>
 using namespace std;
-void swap(int*a,int*b)
+void swap(int* a, int* b)
 {
-    int t=*a;
-    *a=*b;
-    *b=t;
+	int t = *a;
+	*a = *b;
+	*b = t;
 }
-int partition(int arr[],int low,int high)
+
+int partition (int arr[], int low, int high)
 {
-    int pivot=arr[high];
-    int i=(low-1);
-    for(int j=low;j<=high-1;j++)
-    {
-        if(arr[j]<=pivot)
-        {
-            i++;
-            swap(&arr[i],&arr[j]);
-        }
-    }
-    swap(&arr[i+1],&arr[high]);
-    return(i+1);
-} printArray(int arr[],int size)
+	int pivot = arr[high];
+	int i = (low - 1);
+
+	for (int j = low; j <= high- 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+
+void quickSort(int arr[], int low, int high)
 {
-    int i;
-    for(i=0;i<size;i++)
-      cout<<arr[i]<<" ";
-    cout<<"\n";  
+	if (low < high)
+	{
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		cout<<arr[i]<<" ";
+	cout<<"\n";
 }
 
 int main()
 {
     int n=6;
-    in
-
-void quickSort(int arr[],int low,int high)
-{
-    if(low<high)
-    {
-        int pi=partition(arr,low,high);
-        quickSort(arr,low,pi-1);
-    quickSort(arr,pi+1,high);    
-    }
-}
-
-voidt arr[n];
-    for(int i=0;i<n;i++)
-    {   
-       cin>>arr[i];
-    }
-    quickSort(arr,0,n-1);
-    printArray(arr,n);
-    return 0;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];}
+	quickSort(arr, 0, n-1);
+	printArray(arr, n);
+	return 0;
 }
 
 
